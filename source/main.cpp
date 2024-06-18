f2e03dfcbf3b67270d285e14b6210d66aad7acbd
majinwei <majinwei_1234@163.com>
2024-06-18 20:45:18 +0800

    测试了openmp并行以及Eigen求解器的设置

--- source/main.cpp
+++ source/main.cpp
@@ -9,8 +9,51 @@
 #include "dof_map.h"
 #include "post.h"
 #include <omp.h>
+#include <chrono>
 int main(int argc, char *argv[])
 {
+//    omp_set_num_threads(4);
+
+//     const int size = 500000000;
+//     std::vector<int> a(size, 1);
+//     std::vector<int> bb(size, 2);
+//     std::vector<int> c(size, 0);
+
+//     // 记录非并行for循环的开始时间
+//     auto start = std::chrono::high_resolution_clock::now();
+
+//     // 非并行for循环
+//     for (int i = 0; i < size; ++i) {
+//         c[i] = a[i] + bb[i];
+//     }
+
+//     // 记录非并行for循环的结束时间
+//     auto end = std::chrono::high_resolution_clock::now();
+//     std::chrono::duration<double, std::milli> non_parallel_time = end - start;
+
+//     // 打印非并行for循环的时间
+//     std::cout << "Non-parallel for loop time: " << non_parallel_time.count() << " ms" << std::endl;
+
+//     // 清空c向量
+//     std::fill(c.begin(), c.end(), 0);
+
+//     // 记录并行for循环的开始时间
+//     start = std::chrono::high_resolution_clock::now();
+
+//     // 并行化for循环
+//     #pragma omp parallel for
+//     for (int i = 0; i < size; ++i) {
+//         c[i] = a[i] + bb[i];
+//     }
+
+//     // 记录并行for循环的结束时间
+//     end = std::chrono::high_resolution_clock::now();
+//     std::chrono::duration<double, std::milli> parallel_time = end - start;
+
+//     // 打印并行for循环的时间
+//     std::cout << "Parallel for loop time: " << parallel_time.count() << " ms" << std::endl;
+
+//  exit(0);
   std::cout.precision(20);
   Input input(argv[1]); // 读入和解析input文件
   Mesh mesh(input.db["mesh_file_name"][0]); // 读入网格文件
@@ -19,8 +62,8 @@ int main(int argc, char *argv[])
   Post post("aaa");
   post.onlymesh(mesh);
 
-  for (int i = 0; i < mesh.actual_element_count; i++)
-  std::cout << mesh.element_ids[i] << std::endl;
+  // for (int i = 0; i < mesh.actual_element_count; i++)
+  // std::cout << mesh.element_ids[i] << std::endl;
 
 
   // exit(0);
@@ -43,13 +86,13 @@ int main(int argc, char *argv[])
   int row, col;
   double value;
   std::clock_t c_start_readstart = std::clock();
+
+
   while (infile >> row >> col >> value)
   {
     tripletList.push_back(Eigen::Triplet<double>(row, col, value));
   }
   mat.setFromTriplets(tripletList.begin(), tripletList.end());
-
-  std::cout<< mat.coeffRef(0,0) << std::endl;
   Eigen::VectorXd b(tff);
   std::string filename2 = "/home/ma/work/M3D-C/file/b.dat";
   std::ifstream infile2(filename2);
@@ -68,6 +111,8 @@ int main(int argc, char *argv[])
   // Eigen::SimplicialLLT<Eigen::SparseMatrix<double>> solver;
   // Eigen::BiCGSTAB<Eigen::SparseMatrix<double>> solver;
   // Eigen::PardisoLU<Eigen::SparseMatrix<double>> solver;
+  solver.setTolerance(1e+5);     // 设置收敛误差
+  solver.setMaxIterations(10); // 设置最大迭代次数
   solver.compute(mat);
   if (solver.info() != Eigen::Success)
   {
@@ -75,14 +120,7 @@ int main(int argc, char *argv[])
     std::cerr << "分解失败" << std::endl;
     return -1;
   }
-  omp_set_num_threads(4);
-Eigen::VectorXd x;
-#pragma omp parallel
-  {
-#pragma omp single
-    x = solver.solve(b);
-  }
-  // Eigen::VectorXd x = solver.solve(b);
+  Eigen::VectorXd x = solver.solve(b);
   if (solver.info() != Eigen::Success)
   {
     // 求解失败
