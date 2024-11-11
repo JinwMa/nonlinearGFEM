f63cbebabf392045c766e731b8010fdf2fc87226
majinwei <majinwei_1234@163.com>
2024-11-11 22:32:04 +0800

    用于回退的节点

---
+++ source/object/ObjectElementData.h
@@ -0,0 +1,58 @@
+#ifndef OBJECTELEMENTDATA1_H
+#define OBJECTELEMENTDATA1_H
+
+#include<string>
+#include<vector>
+using namespace std;
+struct ObjectElementData
+{
+    // element level
+    bool is_initialized;
+    bool is_alive;
+    int element_id;
+    int element_order;
+    int element_layer;
+    string element_type;
+
+    // node level
+    int num_nodes;
+    vector<int> node_ids;
+    vector<int> element_patch;
+    vector<vector<double>> u;
+    vector<vector<double>> du;
+    vector<vector<double>> ddu;
+    vector<vector<double>> coordinates;
+
+    // integration_points
+    int unm_integration_points;
+    vector<vector<double>>sf;
+    vector<vector<vector<double>>> sfdxyz;
+    vector<vector<vector<double>>> F;
+    vector<vector<vector<double>>> F_n;
+    vector<vector<vector<double>>> F_n1;
+
+    vector<double> jkb;
+    vector<double> jkb_n;
+    vector<double> jkb_n1;
+
+    vector<vector<vector<double>>> Finv;
+    vector<vector<vector<double>>> Finv_n;
+    vector<vector<vector<double>>> Finv_n1;    
+
+    vector<vector<vector<double>>> stress;
+    vector<vector<vector<double>>> stress_n;
+    vector<vector<vector<double>>> stress_n1;   
+
+    vector<double> eff_p_strain;
+    vector<double> eff_p_strain_n;
+    vector<double> eff_p_strain_n1;
+
+};
+
+
+
+
+
+
+
+#endif
